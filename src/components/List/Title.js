9d5efb26fce0177a3cfe40488810356d42e2b5f2
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-25 10:58:08 +0200

    added basic layout

--- src/components/List/Title.js
+++ src/components/List/Title.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, InputBase } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -9,7 +10,17 @@ const useStyle = makeStyles((theme) => ({
     marginLeft: theme.spacing(1),
   },
   editableTitle: {
-    marginLeft: theme.spacing(1),
+    flexGrow: "1",
+  },
+  editableTitleContainer: {
+    margin: theme.spacing(1),
+    display: "flex",
+  },
+  input: {
+    margin: theme.spacing(1),
+    "&: focus": {
+      background: "#ddd",
+    },
   },
 }));
 
@@ -20,16 +31,24 @@ const Title = () => {
     <div>
       {open ? (
         <div>
-          <InputBase value='Todo' />
+          <InputBase
+            value='Todo'
+            inputProps={{
+              className: classes.input,
+            }}
+            fullWidth
+            onBlur={() => setOpen(!open)}
+          />
         </div>
       ) : (
-        <div>
+        <div className={classes.editableTitleContainer}>
           <Typography
             onClick={() => setOpen(!open)}
             className={classes.editableTitle}
           >
             Todo
           </Typography>
+          <MoreHorizIcon />
         </div>
       )}
     </div>
